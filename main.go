aa5652e5afb0e0eeae4973d0f138f06fb6dc9d60
ViktoriaMi <miheykoviktoria@yandex.ru>
2021-12-17 22:40:15 +0300

    2 laba

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,11 +10,44 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	//"github.com/go-redis/redis/v8"
+
+	"github.com/go-redis/redis/v8"
 	//"github.com/gin-gonic/gin"
 	//"github.com/TheBookPeople/iso3166"
 	//"time"
 )
+
+var ctx = context.Background()
+
+func RedisClient() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:8080",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key = ", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+}
 
 type Weather struct {
 	// Coord struct {
@@ -238,6 +272,8 @@ func weather(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf(err3.Error())
 	}
 	fmt.Printf("Weather data in %s by IP:\n%s\n", city_str, string(weathJSON2))
+
+	RedisClient()
 }
 
 func main() {
